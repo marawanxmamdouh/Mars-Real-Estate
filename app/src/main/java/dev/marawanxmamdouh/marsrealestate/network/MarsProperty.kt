7902c944c7fdb8a43f4ea874b31fa27c29a14361
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-08 02:05:43 +0200

    Apply Parcelize to the data class

--- app/src/main/java/dev/marawanxmamdouh/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/dev/marawanxmamdouh/marsrealestate/network/MarsProperty.kt
@@ -1,7 +1,9 @@
 package dev.marawanxmamdouh.marsrealestate.network
 
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MarsProperty(
     val id: String,
     @Json(name = "img_src") val imgSrcUrl: String,
